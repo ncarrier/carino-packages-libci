22dbc6c2dc074ed1fb1da5959b74ec72e8250d28
Nicolas CARRIER <carrier.nicolas0@gmail.com>
2015-03-06 06:10:00 +0100

    placed libci under the LGPL

--- src/ci_command.c
+++ src/ci_command.c
@@ -1,3 +1,21 @@
+/*
+ * This file is part of libci.
+ *
+ *  libci is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  libci is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with libci.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2015 Nicolas CARRIER <carrier dot nicolas0 at gmail dot com>
+ */
 /**
  * @file ci_command.c
  * @brief Implementation of the communication protocol between a vehicle and the
